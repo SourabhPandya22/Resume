1faf78f430b3122210df919f8029dfcf53cfa5f1
Sourabh Pandya <sourabhpandya.0@gmail.com>
2026-01-22 13:10:24 +0530

    Resume Upload

---
+++ assets/js/general.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    var headerHeight = $('.navbar').outerHeight() || 0;
+
+    // CLICK SMOOTH SCROLL
+    $('.nav-link').on('click', function (e) {
+        e.preventDefault();
+
+        var target = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - headerHeight
+        }, 600);
+    });
+
+    // SCROLL ACTIVE CLASS
+    $(window).on('scroll', function () {
+        var scrollPos = $(window).scrollTop() + headerHeight + 10;
+
+        $('.nav-link').each(function () {
+            var currLink = $(this);
+            var section = $(currLink.attr('href'));
+
+            if (
+                section.length &&
+                section.offset().top <= scrollPos &&
+                section.offset().top + section.outerHeight() > scrollPos
+            ) {
+                $('.nav-link').removeClass('active');
+                currLink.addClass('active');
+            }
+        });
+    });
+});
